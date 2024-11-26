9030a3fa0367c688a028ea87ba9cadd1aec8b9a6
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-11-25 21:24:26 -0300

    mds era só mudar uma coisinha q ia ter me salvado 36 anos

--- mobile/spotfake/scripts/LoginContext.jsx
+++ mobile/spotfake/scripts/LoginContext.jsx
@@ -5,7 +5,7 @@ export const LoginContext = createContext();
 export const LoginProvider = ({ children }) => {
     const [token, setToken] = useState('');
     const [foto, setFoto] = useState('https://placeholder.pics/svg/300');
-    const [userData, setUserData] = useState('')
+    const [userData, setUserData] = useState({})
 
     return (
         <LoginContext.Provider value={{ token, setToken, foto, setFoto, userData, setUserData }}>
